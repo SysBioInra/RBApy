52cc884c2829bc41b00fdcc39f973f3a921362db
sfischer <sfischer@728e8fc7-79d9-4508-83f3-7bea08a47735>
2017-05-31 12:43:38 +0000

    MAJOR ----- Added agregates to model parameters. They are used to define functions as a multiplication of existing functions.

--- trunk/rba/rba/rba_xml.py
+++ trunk/rba/rba/rba_xml.py
@@ -311,11 +311,13 @@ class RbaParameters(object):
     def __init__(self):
         self.maximal_densities = ListOfMaximalDensities()
         self.functions = ListOfFunctions()
+        self.aggregates = ListOfAggregates()
 
     def write(self, output_stream, doc_name = 'RBAParameters'):
         root = etree.Element(doc_name)
         root.extend([self.maximal_densities.to_xml_node(),
-                     self.functions.to_xml_node()])
+                     self.functions.to_xml_node(),
+                     self.aggregates.to_xml_node()])
         etree.ElementTree(root).write(output_stream, pretty_print=True)
 
     @classmethod
@@ -326,6 +328,8 @@ class RbaParameters(object):
         result.maximal_densities = ListOfMaximalDensities.from_xml_node(n)
         n = get_unique_child(node, ListOfFunctions.tag)
         result.functions = ListOfFunctions.from_xml_node(n)
+        n = get_unique_child(node, ListOfAggregates.tag)
+        result.aggregates = ListOfAggregates.from_xml_node(n)
         return result
     
 class MaximalDensity(ValueNode):
@@ -349,6 +353,52 @@ class MaximalDensity(ValueNode):
 class ListOfMaximalDensities(ListOf):
     tag = 'listOfMaximalDensities'
     list_element = MaximalDensity
+
+class FunctionReference(object):
+    tag = 'functionReference'
+
+    def __init__(self, function):
+        self.function = function
+
+    def to_xml_node(self):
+        result = etree.Element(self.tag)
+        result.set('function', self.function)
+        return result
+
+    @classmethod
+    def from_xml_node(cls, node):
+        return cls(node.get('function'))
+
+class ListOfFunctionReferences(ListOf):
+    tag = 'listOfFunctionReferences'
+    list_element = FunctionReference
+    
+class Aggregate(object):
+    tag = 'aggregate'
+
+    def __init__(self, id_, type_):
+        if id_ is not None: self.id = id_
+        else: self.id = ''
+        self.type = type_
+        self.function_references = ListOfFunctionReferences()
+
+    def to_xml_node(self):
+        result = etree.Element(self.tag)
+        result.set('id', self.id)
+        result.set('type', self.type)
+        result.extend([self.function_references.to_xml_node()])
+        return result
+
+    @classmethod
+    def from_xml_node(cls, node):
+        result = cls(node.get('id'), node.get('type'))
+        n = get_unique_child(node, listOfFunctionReferences.tag)
+        result.parameters = ListOfFunctionReferences.from_xml_node(n)
+        return result
+
+class ListOfAggregates(ListOf):
+    tag = 'listOfAggregates'
+    list_element = Aggregate
 
 ################################################################################
 #                               MACROMOLECULES                                 #
