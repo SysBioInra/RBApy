a3a852d63c21b1168dd46b2ad04473033237ba38
sfischer <sfischer@728e8fc7-79d9-4508-83f3-7bea08a47735>
2017-06-20 12:56:47 +0000

    Refactoring code to match python standards. Substantial refactoring in the cofactor.py module to make the class easier to reuse.

--- trunk/rba/rba/pre_rba/pipeline_parameters.py
+++ trunk/rba/rba/pre_rba/pipeline_parameters.py
@@ -1,14 +1,26 @@
+"""
+Module defining PipelineParameters class.
+"""
 
-class PipelineParameters:
+# python 2/3 compatibility
+from __future__ import absolute_import, division, print_function
+
+class PipelineParameters(object):
     """
     Class storing pipeline parameters.
+
+    Attributes:
+        obligatory_tags: tags that a parameter file must contain.
+        optional_tags: tags that a parameter file may contain.
+        parameters: dict mapping parameter tags with their value. Optional tags
+            may be omitted.
     """
     def __init__(self, parameter_file):
         """
-        Constructor.
+        Constructor from file.
 
-        :param parameter_file: path to parameter file.
-        :type parameter_file: string
+        Attributes:
+            parameter_file: path to parameter file.
         """
         self.obligatory_tags = ['INPUT_DIR', 'OUTPUT_DIR', 'SBML_FILE',
                                 'ORGANISM_ID']
@@ -16,18 +28,19 @@ class PipelineParameters:
         self.parameters = {}
         
         try:
-            with open(parameter_file, 'rU') as f:
+            with open(parameter_file, 'rU') as input_stream:
                 # parse file
-                for line in f:
+                for line in input_stream:
                     line = line.strip()
-                    if line == '' or line.startswith('#'): continue
+                    if line == '' or line.startswith('#'):
+                        continue
                     try:
-                        tag, value = map(str.strip, line.split('='))
+                        tag, value = [word.strip() for word in line.split('=')]
                     except ValueError:
                         print ('Invalid parameter format:\n' + line)
                         raise UserWarning('Invalid parameter file.')
-                    if (tag in self.obligatory_tags) \
-                       or (tag in self.optional_tags):
+                    if ((tag in self.obligatory_tags)
+                        or (tag in self.optional_tags)):
                         self.parameters[tag] = value
                     else:
                         print('Warning: ignoring unknown parameter '
@@ -35,11 +48,11 @@ class PipelineParameters:
         except IOError:
             print('Could not find parameter file ' + parameter_file + '.')
             raise UserWarning('Invalid parameter file.')
-            
+        
         # check that all obligatory tags have been found
         missing_parameters = [tag for tag in self.obligatory_tags
                               if tag not in self.parameters.keys()]
-        if len(missing_parameters)>0:
+        if len(missing_parameters) > 0:
             print('Following parameters are missing: '
                   + ', '.join(missing_parameters))
             raise UserWarning('Invalid parameter file.')
--- trunk/rba/rba/pre_rba/uniprot_filter.py
+++ trunk/rba/rba/pre_rba/uniprot_filter.py
@@ -1,71 +1,67 @@
+"""
+Module defining UniprotFilter class.
+"""
 
-# make division python3 style (float division), i.e. 3 / 2 = 1.5
-from __future__ import division
+# python 2/3 compatibility
+from __future__ import absolute_import, division, print_function
 
-import pandas, os.path
+# global imports
+import os.path
 import re
+import pandas
 
-from ..rba_xml import *
-from .cofactor import *
-from .subunit import *
-from .location import *
-from .unknown_proteins import *
+# local imports
+from rba import rba_xml
+from rba.pre_rba.cofactor import CofactorData
+from rba.pre_rba.subunit import SubunitData
+from rba.pre_rba.location import LocationData
+from rba.pre_rba.unknown_proteins import UnknownProteins
 
-class UniprotFilter:
+class UniprotFilter(object):
     """
     Class used to filter RBA-relevant data from a uniprot file.
 
-    :param input_dir: Directory containing uniprot and helper files.
-    :param gene_ids: Genes which information needs to be retrieved. If nothing
-    is specified, everything is kept.
-    :type input_dir: string
-    :type gene_ids: list of strings
+    Attributes:
+        unknown_map: dict mapping sbml genes not retrieved in uniprot with
+            a corresponding user-defined gene theoretically found in uniprot.
+        default_location: name of default compartment.
+        cytoplasm_compartment: name of cytoplasm.
+        secreted_compartment: name of external compartment.
+        compartment_ids: list of names of all compartments.
+        components: rba_xml.ListOfComponents containing all elements that are
+            used to build proteins (amino acids, vitamins, cofactors).
+        proteins: rba_xml.ListOfMacromolecules containing all proteins that
+            have been filtered.
+        protein_stoichiometry: dict mapping protein identifiers with their
+            stoichiometry within their enzymatic complex.
+        average_protein_length: average protein length.
     """
-    def __init__(self, gene_ids, input_dir = '.'):
-        """
-        Constructor.
 
-        :param gene_ids: list of proteins that need to be retrieved.
-        :param input_dir: path to folder containing uniprot files.
-        :type gene_ids: list of strings
-        :type input_dir: string
+    def __init__(self, gene_ids, input_dir='.'):
         """
-        self._gene_ids = list(set(gene_ids))
-        self._upper_gene_ids = list(map(str.upper, self._gene_ids))
-        self._gene_id_found = [False] * len(self._gene_ids)
-        
+        Constructor from input directory and gene identifiers.
+
+        Args:
+            input_dir: Directory containing uniprot and helper files.
+            gene_ids: Genes to retrieve. If nothing is specified, everything
+                is kept.
+        """
         # open uniprot data
         input_file = os.path.join(input_dir, 'uniprot.csv')
-        data = pandas.read_csv(input_file, sep = '\t')
-        # open helper file for unknown proteins
+        data = pandas.read_csv(input_file, sep='\t')
+        # add column containing identifier of protein as found in gene_ids
         unknown_proteins = UnknownProteins(input_dir)
         self.unknown_map = unknown_proteins.data
-        # replace ids marked as unknown by user-define counterpart
-        for i, gene in enumerate(gene_ids):
-            try:
-                gene_ids[i] = self.unknown_map[gene]
-            except KeyError: pass
-            
-        # transform gene data into list
-        self._gene_reader = re.compile(r'([^\s]+)')
-        data['SBML name'] = data['Gene names'].apply(self._genes_as_list)
-        
-        # keep only ids that appear in the SBML file or helper file
-        data['SBML name'] = data['SBML name'].apply(self._filter_genes)
-
-        # update helper file for unknown proteins
-        not_found = []
-        for gene_id, found in zip(self._gene_ids, self._gene_id_found):
-            if not(found):
-                not_found.append(gene_id)
+        data['Lookup names'], not_found \
+            = self._lookup_names(data['Gene names'], gene_ids, self.unknown_map)
         unknown_proteins.add(not_found)
 
         # remove entries for which there is no known id
         drop_rows = []
-        gene_data = data['SBML name']
-        for i, gene_names in enumerate(gene_data):
+        gene_data = data['Lookup names']
+        for index, gene_names in enumerate(gene_data):
             if len(gene_names) == 0:
-                drop_rows.append(i)
+                drop_rows.append(index)
         reduced_data = data.drop(drop_rows)
 
         # get cofactor, subunit and location data
@@ -80,10 +76,10 @@ class UniprotFilter:
 
         # gather protein compositions in terms of amino acids
         aa_data = self._aa_data(data)
-            
+
         # store data
-        self.components = ListOfComponents()
-        self.proteins = ListOfMacromolecules()
+        self.components = rba_xml.ListOfComponents()
+        self.proteins = rba_xml.ListOfMacromolecules()
         self.protein_stoichiometry = {}
         # add amino acids to component set
         amino_acids = set()
@@ -91,10 +87,10 @@ class UniprotFilter:
             for aa in comp:
                 amino_acids.add(aa)
         for aa in amino_acids:
-            self.components.append(Component(aa, '', 'amino_acid', 1))
+            self.components.append(rba_xml.Component(aa, '', 'amino_acid', 1))
         # store protein data
         entry_data = reduced_data['Entry']
-        gene_data = reduced_data['SBML name']
+        gene_data = reduced_data['Lookup names']
         known_cofactors = []
         for entry, gene_names in zip(entry_data, gene_data):
             # add amino acids to composition
@@ -103,20 +99,24 @@ class UniprotFilter:
             cofactors = []
             if entry in cofactor_data:
                 cofactors = cofactor_data[entry]
-            for c in cofactors:
-                composition[c.chebi] = c.stoichiometry
-                if c.chebi not in known_cofactors:
-                    self.components.append(Component(c.chebi, c.name,
-                                                     'cofactor', 0))
-                    known_cofactors.append(c.chebi)
+            for cofactor in cofactors:
+                composition[cofactor.chebi] = cofactor.stoichiometry
+                if cofactor.chebi not in known_cofactors:
+                    new_cofactor = rba_xml.Component(cofactor.chebi,
+                                                     cofactor.name,
+                                                     'cofactor', 0)
+                    self.components.append(new_cofactor)
+                    known_cofactors.append(cofactor.chebi)
             # check if protein is referenced in SBML with different ids (!!!)
             if len(gene_names) > 1:
                 print('In your SBML, ids ' + ', '.join(gene_names)
                       + ' reference the same protein. Protein will be '
                       + 'duplicated for now. Check your SBML for future runs.')
+            # add protein(s) to list
             for gene_name in gene_names:
-                self.proteins.append \
-                    (Macromolecule(gene_name, location_data[entry], composition))
+                protein = rba_xml.Macromolecule(gene_name, location_data[entry],
+                                                composition)
+                self.proteins.append(protein)
                 self.protein_stoichiometry[gene_name] = subunit_data[entry]
 
         # compute average protein
@@ -131,43 +131,83 @@ class UniprotFilter:
         # add one average protein for every compartment
         for location in self.compartment_ids:
             name = 'average_protein_' + location
-            self.proteins.append \
-                (Macromolecule(name, location, average_protein))
+            protein = rba_xml.Macromolecule(name, location, average_protein)
+            self.proteins.append(protein)
             self.protein_stoichiometry[name] = 1
 
-    def _genes_as_list(self, gene_names):
+    @staticmethod
+    def _lookup_names(uniprot_names, gene_ids, unknown_map):
         """
-        Transforms gene data in a format that is easier to work on.
+        Append a column to data containing lookup name of protein.
+
+        Args:
+            uniprot_names: column containing raw uniprot 'Gene names' fields.
+            gene_ids: name of genes to retrieve.
+            unknown_map: dict where keys are genes that could not be 
+                retrieved in 'Gene names'. Values is an alternative identifier
+                which might be found in 'Gene names'.
+
+        Returns:
+            First element: Column containing list of genes found on every
+                row of input column, using names in gene_ids.
+                Example:
+                    uniprot_names = [['gene_a gene_a_2'],
+                                     ['gene_b]]
+                    gene_ids = ['GENE_A', 'GENE_C']
+                    output -> [['GENE_A'],
+                               []]
+            Second element: List of ids that could not be retrieved.
         """
-        # no gene name associated with protein
-        if pandas.isnull(gene_names): return []
+        gene_ids = list(set(gene_ids))
+        ## replace ids by user-defined counterpart if applicable
+        for index, name in enumerate(gene_ids):
+            user_id = unknown_map.get(name, None)
+            if user_id is not None:
+                gene_ids[index] = user_id
+        ## create a mapping with standardized upper case names
+        original_gene_ids = {name.upper(): name for name in gene_ids}
+        ## match uniprot field with genes to retrieve
+        gene_reader = re.compile(r'([^\s]+)')
+        lookup_names = uniprot_names.copy()
+        gene_found = {name: False for name in gene_ids}
+        for row_index, row in enumerate(lookup_names):
+            # transform raw uniprot field into standardized list
+            if pandas.isnull(row):
+                gene_list = []
+            else:
+                gene_list = list(set(gene_reader.findall(row)))
+            # keep only ids that appear in the list of ids to retrieve
+            curated_ids = []
+            for name in gene_list:
+                original_name = original_gene_ids.get(name.upper(), None)
+                if original_name is not None:
+                    curated_ids.append(original_name)
+                    gene_found[original_name] = True
+            lookup_names[row_index] = curated_ids
+        ## establish list of names that could not be found
+        not_found = [name for name, found in gene_found.items() if not found]
+        return lookup_names, not_found
 
-        # transform names into a list
-        return list(set(map(str.upper, self._gene_reader.findall(gene_names))))
+    @staticmethod
+    def _aa_data(uniprot_data):
+        """
+        Retrive amino acid composition of proteins.
 
-    def _filter_genes(self, gene_ids):
-        # filter known ids
-        # we expect to keep at most one !!!
-        curated_ids = []
-        for i in gene_ids:
-            try:
-                index = self._upper_gene_ids.index(i)
-                curated_ids.append(self._gene_ids[index])
-                self._gene_id_found[index] = True
-            except ValueError:
-                pass
-        return curated_ids
+        Args:
+            uniprot_data: pandas table containing protein information.
 
-    def _aa_data(self, uniprot_data):
+        Returns:
+            dict mapping uniprot entry to its composition. Composition is a
+            dict mapping amino acids with their occurence in the sequence.
+        """
         entry_data = uniprot_data['Entry']
         sequence_data = uniprot_data['Sequence']
         aa_data = {}
         # read sequence composition for every entry
         for entry, sequence in zip(entry_data, sequence_data):
             new_data = {}
-            for c in sequence:
-                if c not in new_data:
-                    new_data[c] = 0
-                new_data[c] += 1
+            for char in sequence:
+                new_data.setdefault(char, 0)
+                new_data[char] += 1
             aa_data[entry] = new_data
         return aa_data
