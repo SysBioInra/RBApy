535b2ce1c581c15e7945e17e899dcc7376c30496
sfischer <30040612+yexilein@users.noreply.github.com>
2018-01-26 12:09:22 +0000

    Added a utility directory to the package.

--- rba/__init__.py
+++ rba/__init__.py
@@ -9,8 +9,10 @@ from .results import Results
 from .xml import RbaModel
 from .prerba import *
 from .core import *
+from .utils import *
 
 from . import prerba, core
 __all__ = ['RbaModel', 'Results']
 __all__ += prerba.__all__
 __all__ += core.__all__
+__all__ += utils.__all__
