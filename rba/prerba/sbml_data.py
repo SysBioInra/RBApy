1369b6d65cd05ac06d6cca9c6ab1d1c7d2c8686e
Stephan Fischer <stephischer@gmail.com>
2018-04-13 09:53:52 -0400

    Fix parsing bug in COBRA notes

--- rba/prerba/sbml_data.py
+++ rba/prerba/sbml_data.py
@@ -255,8 +255,10 @@ class CobraNoteParser(object):
     def _parse_gene_association(self, text):
         """We assume that relations are always 'or's of 'and's."""
         tags = text.split(':', 1)
-        if len(tags) != 2 and tags[0] != "GENE_ASSOCIATION":
+        if len(tags) != 2:
             print('Invalid note field: ' + text)
+            return None
+        if tags[0] != "GENE_ASSOCIATION":
             return None
         enzyme_description = self._remove_parentheses(tags[1])
         if not enzyme_description:
