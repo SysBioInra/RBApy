be11e18c1444c11303c4e3dab254be68e8689eeb
obodeit <38722594+obodeit@users.noreply.github.com>
2022-09-29 17:07:29 +0200

    Fixed authors import from file

--- rba/__init__.py
+++ rba/__init__.py
@@ -5,13 +5,17 @@ RBA package
 Package computing Resource Balance Analysis.
 """
 
-from ._version import __version__  # noqa: F401
 from .model import RbaModel  # noqa: F401
 from .prerba import *  # noqa: F401
 from .core import *  # noqa: F401
 from .utils import *  # noqa: F401
 
 from . import prerba, core, utils  # noqa: F401
+
+from pkg_resources import resource_string
+
+__version__ = resource_string(__name__, '_version.py').decode("utf-8").split("'")[1]
+__author__ = resource_string(__name__, '_authors.py').decode("utf-8").split("'")[1]
 
 __all__ = ['RbaModel']
 __all__ += prerba.__all__
